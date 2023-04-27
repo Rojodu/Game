2e99b466597f855f61e0ed174f6268e6c2ca50b9
rojodu <robjdupont@gmail.com>
2023-04-26 22:09:43 -0500

    more pygame stuff

---
+++ Chapter_Eight/Squirrel_Eat_Squirrel.py
@@ -0,0 +1,19 @@
+# Squirrel Eat Squirrel (a 2D Katamari Damacy clone)
+# By Al Sweigart al@inventwithpython.com
+# http://inventwithpython.com/pygame
+# Creative Commons BY-NC-SA 3.0 US
+
+import random, sys, time, math, pygame
+from pygame.locals import *
+
+FPS = 30
+WINWIDTH = 640
+WINHEIGHT = 480
+HALF_WINWIDTH = int(WINWIDTH / 2)
+HALF_WINHEIGHT = int(WINHEIGHT / 2)
+
+GRASSCOLOR = (24, 255, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+CAMERASLACK = 90 
